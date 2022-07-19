cdd742e132f6d9198f87cef8e6e059358c1bc5cf
yuvaraj82 <108718010+yuvaraj82@users.noreply.github.com>
2022-07-19 06:51:38 +0530

    Update SI.java

--- Assessment/src/main/java/week1/SI.java
+++ Assessment/src/main/java/week1/SI.java
@@ -2,17 +2,17 @@ package week1;
 
 public class SI {
 	static int principleAmount;
-	static int rateInterest;
+	static int rateOfInterest;
 	static int tenure;
 	static int simpleInterest;
 	static int totalAmount;
 	
-	void simpleInterest(int principleAmount,int rateInterest,int tenure)
+	void simpleInterest(int principleAmount,int rateOfInterest,int tenure)
 	{
-		simpleInterest=(principleAmount*rateInterest*tenure)/100;
+		simpleInterest=(principleAmount*ratOfInterest*tenure)/100;
 		System.out.println("Simple Interest is= "+simpleInterest);
 		totalAmount=(principleAmount+simpleInterest);
-		System.out.println("Total amount to be paid= "+totalAmount);
+		System.out.println("Total amount to be paid after loan duration ends= "+totalAmount);
 		
 		
 	}
