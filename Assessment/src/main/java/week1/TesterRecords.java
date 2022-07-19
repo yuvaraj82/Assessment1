52ffc1c6d5b5b4ad44c8ce468621732ebefbd24e
yuvaraj82 <108718010+yuvaraj82@users.noreply.github.com>
2022-07-19 06:38:25 +0530

    Update and rename TestSchoolRecords.java to TesterRecords.java

---
+++ Assessment/src/main/java/week1/TesterRecords.java
@@ -0,0 +1,18 @@
+package week1;
+
+public class TesterRecords {
+
+	public static void main(String[] args) {
+		SchoolRecords sr=new SchoolRecords();
+		sr.addSchoolDetails(1117,"RMK matriculation school","Chennai");
+		sr.dispSchoolDetails();
+		sr.addSchoolDetails(1116,"Velammal matriculation school","Chennai");
+		sr.dispSchoolDetails();
+		sr.addSchoolDetails(1115,"SBOA matriculation school","Chennai");
+		sr.dispSchoolDetais();
+		
+
+
+	}
+
+}
